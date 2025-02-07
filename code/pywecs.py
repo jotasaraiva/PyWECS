62faf9605b0bf3c7c70d482f4c46b94e384f1a81
jotasaraiva <jpsaraiva2002@gmail.com>
2025-02-07 10:28:08 -0300

    Adds Kittler license

--- code/pywecs.py
+++ code/pywecs.py
@@ -64,29 +64,6 @@ def WECS(x, print_progress=True):
     x_wecs = ECS(x, smooth_x=x_denoised)
     return x_wecs
 
-def threshold_ki(img, min_val=0, max_val=1):
-    """
-    The reimplementation of Kittler-Illingworth Thresholding algorithm by Bob Pepin.
-    Original Matlab code: https://www.mathworks.com/matlabcentral/fileexchange/45685-kittler-illingworth-thresholding
-    Paper: Kittler, J. & Illingworth, J. Minimum error thresholding. Pattern Recognit. 19, 41–47 (1986).
-    """
-    h, g = np.histogram(img.ravel(), 265, [min_val, max_val])
-    g = g[:-1]
-    c = np.cumsum(h)
-    m = np.cumsum(h * g)
-    s = np.cumsum(h * g**2)
-    sigma_f = np.sqrt(s/c - (m/c)**2)
-    cb = c[-1] - c
-    mb = m[-1] - m
-    sb = s[-1] - s
-    sigma_b = np.sqrt(sb/cb - (mb/cb)**2)
-    p =  c / c[-1]
-    v = p * np.log(sigma_f) + (1-p)*np.log(sigma_b) - p*np.log(p) - (1-p)*np.log(1-p)
-    v[~np.isfinite(v)] = np.inf
-    idx = np.argmin(v)
-    t = g[idx]
-    return t
-
 def segment2d(x, method='ot'):
     assert method in ('ot', 'ki', 'km'), 'Method needs to be Otsu (ot), Kittler-Illingworth (ki) or KMeans (km)'
     if method == 'ot':
@@ -103,3 +80,54 @@ def segment2d(x, method='ot'):
         kmeans = KMeans(n_clusters=2, random_state=0).fit(x.reshape(-1,1))
         res = kmeans.cluster_centers_[kmeans.labels_].reshape(x.shape)
         return res
+    
+# Adapted from https://github.com/al42and/cv_snippets/blob/master/kittler.py
+# Copyright (c) 2014, Bob Pepin
+# Copyright (c) 2016, Andrey Alekseenko
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the distribution
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+def threshold_ki(img, min_val=0, max_val=1):
+    """
+    The reimplementation of Kittler-Illingworth Thresholding algorithm by Bob Pepin.
+    Original Matlab code: https://www.mathworks.com/matlabcentral/fileexchange/45685-kittler-illingworth-thresholding
+    Paper: Kittler, J. & Illingworth, J. Minimum error thresholding. Pattern Recognit. 19, 41–47 (1986).
+    """
+    h, g = np.histogram(img.ravel(), 265, [min_val, max_val])
+    g = g[:-1]
+    c = np.cumsum(h)
+    m = np.cumsum(h * g)
+    s = np.cumsum(h * g**2)
+    cb = c[-1] - c
+    mb = m[-1] - m
+    sb = s[-1] - s
+    with np.errstate(invalid='ignore', divide='ignore'):
+        sigma_f = np.sqrt(s/c - (m/c)**2)
+        sigma_b = np.sqrt(sb/cb - (mb/cb)**2)
+        p = c / c[-1]
+        v = p * np.log(sigma_f) + (1-p)*np.log(sigma_b) - p*np.log(p) - (1-p)*np.log(1-p)
+    v[~np.isfinite(v)] = np.inf
+    idx = np.argmin(v)
+    t = g[idx]
+    return t
